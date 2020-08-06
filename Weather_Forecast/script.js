670dc85e976004d8f1affad44c1d74dfe7a6e9ef
briansun <imbriansun@gmail.com>
2020-08-06 16:59:34 +0800

    Basic Weather Forecast Version -- v0.1

---
+++ Weather_Forecast/script.js
@@ -0,0 +1,55 @@
+function handleResponse(response){
+    // Handling backgroundImg
+    switch (response.result.weather){
+        case "晴":
+            document.body.style.backgroundImage = 'url("https://s1.ax1x.com/2020/08/06/agwbgH.png")';
+            break;
+        case "多云":
+        case "阴":
+        case "雾":
+            document.body.style.backgroundImage = 'url("https://s1.ax1x.com/2020/08/06/agwMnI.png")';
+            break;
+        case "雷阵雨":
+        case "小雨":
+        case "中雨":
+        case "大雨":
+            document.body.style.backgroundImage = 'url("https://s1.ax1x.com/2020/08/06/ag0ivj.png")';
+            break;
+        default:
+            document.body.style.backgroundImage = 'url("https://s1.ax1x.com/2020/08/06/ag0ILn.png")';
+            break;
+    }
+    // Handling intradayWeatherInfo
+    var cityHeader = document.getElementById("cityHeader");
+    var temperatureElement = document.getElementById("temperature");
+    var weatherDescriptionHeader = document.getElementById("weatherDescriptionHeader");
+    var windSpeedHeader = document.getElementById("windSpeed");
+    var humidityElement = document.getElementById("humidity");
+    var weatherIcon = document.getElementById("documentIconImg");
+
+    weatherIcon.src = "https://www.jisuapi.com/weather/static/images/weathercn/"+response.result.img+".png";
+
+    cityHeader.innerHTML = response.result.city;
+    weatherDescriptionHeader.innerText = response.result.weather;
+    temperatureElement.innerHTML = Math.floor(response.result.temp) + "&#176";
+    windSpeedHeader.innerHTML = response.result.winddirect + " " + response.result.windpower;
+    humidityElement.innerHTML = "湿度：" + response.result.humidity + "%";
+
+}
+
+
+
+function searchWeather(searchTerm){
+    var script = document.createElement("script");
+    script.src = "https://api.jisuapi.com/weather/query?appkey=bd87493350ec7055&city="+searchTerm+"&callback=handleResponse";
+    document.body.insertBefore(script, document.body.firstChild);
+}
+
+
+document.getElementById("searchBtn").addEventListener("click", function(){
+    var searchTerm = document.getElementById("searchInput").value;
+    if(searchTerm){
+        searchWeather(searchTerm);
+    }
+})
+
